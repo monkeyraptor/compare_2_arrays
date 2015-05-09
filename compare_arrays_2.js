3be62efed196b69b2fba8139b7b53e5e827d1640
Monkey Raptor <paul.johanov@gmail.com>
2015-05-09 21:48:30 +0700

    Using modern-er methods
    Such as: *filter* and *forEach*, plus switched the *for* into *do-while*.

---
+++ compare_arrays_2.js
@@ -0,0 +1,55 @@
+function c_a(a, b) {
+    "use strict";
+    var c,
+        d,
+        l_0,
+        l_1,
+        obj = {"difference": [], "same_elements": []};
+
+        function trim(a) {
+            var result;
+            function p(v) {
+                if (!v.toString().match(/\(\*\)/g)) {
+                    return false;
+                }
+                return true;
+            }
+            result = a.filter(p);
+            return result;
+        }
+
+        function splice_it(k, l, m, n) {
+            var i = 0,
+                j,
+                buffer = [];
+
+            do {
+                j = 0;
+                do {
+                    if (l[j] === k[i]) {
+                        buffer.push(l[j]);
+                        l.splice(j, 1, l[j] + " (*)");
+                        k.splice(i, 1, l[i] + " (*)");
+                    }
+                    j++;
+                } while (j < n);
+                i++;
+            } while (i < m);
+            
+            return {"difference": trim(k).concat(trim(l)), "same_elements": buffer};
+        }
+        
+    if (typeof(a) === "object" && typeof(b) === "object") {
+        c = a.slice();
+        d = b.slice();
+        l_0 = c.length;
+        l_1 = d.length;        
+        
+        if (c.length >= d.length) {
+            obj = splice_it(c, d, l_0, l_1);
+        } else {
+            obj = splice_it(d, c, l_1, l_0);
+        }                
+    }
+    return obj;
+}
